67b8edd41fcb9f7a1500cc49c864f18f8d842603
AZZAM UDDIN <uazzam69@gmail.com>
2024-11-16 00:01:27 +0530

    Added navigation menu from shadcn to header

--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -1,4 +1,19 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import Logo from "../../public/cypresslogo.svg";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
 
 const routes = [
   {
@@ -19,8 +34,35 @@ const routes = [
   },
 ];
 
+const components: { title: string; href: string; description: string }[] = [];
+
 const header = ({}) => {
-  return <div></div>;
+  const [path, setPath] = useState("#products");
+  return (
+    <header className="p-4 flex justify-center items-center">
+      <Link href="/" className="w-full flex gap-2">
+        <Image src={Logo} alt="Logo" width={25} height={25} />
+        <span className="font-semibold dark:text-white">Notion.</span>
+      </Link>
+      <NavigationMenu className="hidden md:block">
+        <NavigationMenuList className="gap-6">
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              onClick={() => setPath("#resources")}
+              className={cn({
+                "dark:text-white": path === "#resources",
+                "dark:text-white/40": path !== "#resources",
+                "font-normal": true,
+                "text-xl": true,
+              })}
+            >
+              Resources
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
 };
 
 export default header;
